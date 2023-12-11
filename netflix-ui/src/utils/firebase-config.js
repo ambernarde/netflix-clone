24a0277a07bef06fa348e3c00d490fc5ecd36c68
Andre Miranda Bernarde <ambernarde@gmail.com>
2023-12-10 22:10:15 -0300

    Update parameters

--- netflix-ui/src/utils/firebase-config.js
+++ netflix-ui/src/utils/firebase-config.js
@@ -4,13 +4,7 @@ import {getAuth} from "firebase/auth";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCAImanWzMQFZ4gMYqfOjY9e_fzEUg0dAw",
-  authDomain: "react-netflix-clone-ed450.firebaseapp.com",
-  projectId: "react-netflix-clone-ed450",
-  storageBucket: "react-netflix-clone-ed450.appspot.com",
-  messagingSenderId: "259359341745",
-  appId: "1:259359341745:web:f65de4e36d97efdb2af690",
-  measurementId: "G-1T1F7FVEW5"
+
 };
 
 // Initialize Firebase
